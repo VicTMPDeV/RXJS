e121183d816bd394dc7e007ab13cef7bc079da86
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-06-27 09:04:58 +0200

    create Observable over a single DOM element and subscribe to his click events

--- 04-rxjs-mapto-map-and-filter/src/sandbox.js
+++ 04-rxjs-mapto-map-and-filter/src/sandbox.js
@@ -1,9 +1,21 @@
 import { displayLog } from './utils';
+//1.- Importamos fromEvent de rxjs para capturar los eventos del Grid
+import { fromEvent } from 'rxjs';
+
+// ####################################################################################################################
+// Operador mapTo() de RxJS
+// --------------------------------------------------------------------------------------------------------------------
+// Es el Operador de Transformación más simple de RxJS.
+// Para cada Evento que entra, transforma la salida, devolviendo SIEMPRE EL MISMO VALOR.
+// ####################################################################################################################
 
 export default () => {
-    /** start coding */
+    //2.- Accedo al elemento del DOM sobre el que quiero trabajar.
     const grid = document.getElementById('grid');
+    //3.- Creamos un Observable de sus Eventos Click y nos suscribimos.
+    const clickGridObs$ = fromEvent(grid, 'click');
+    const subscription = clickGridObs$.subscribe( evt => console.log(evt) );
 
 
-    /** end coding */
+
 }
